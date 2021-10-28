e431443c16e674fa0ba626abfa49f681d4ac7531
isabella-df <93315810+isabella-df@users.noreply.github.com>
2021-10-28 11:39:42 +0200

    Update bacteria_Growth.py
    pushing to try and access file 'test'

--- bacteria_Growth.py
+++ bacteria_Growth.py
@@ -4,9 +4,16 @@ Created on Thu Oct 28 10:29:57 2021
 
 @author: isabe
 """
-
+import pandas as pd
 import numpy as np
 
 #why do birds suddenly appearrrrrr
 
-def dataLoad()
+def dataLoad(filename):
+    file = open(filename, 'r')
+    data = []
+    for row in file:
+        data.append([int(x) for x in row.split()])
+    return data
+
+print(dataLoad(test))
