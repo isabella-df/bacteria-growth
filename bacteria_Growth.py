1dd01505cfb5c027cb5c06788d7291ece8d03449
isabella-df <93315810+isabella-df@users.noreply.github.com>
2021-10-28 16:56:14 +0200

    Code for the second part of the project!
    A set of if statements for the string "statistic". Taking the mean and  standard deviation of temperature and growth rate columns. Finding the number of rows by taking the length of the array. Finally, finding the mean growth rate when the temperature is < 20  by creating a new array where only rows where temp is < 20 exist. Then do the same for when temp is >50.

--- bacteria_Growth.py
+++ bacteria_Growth.py
@@ -25,3 +25,28 @@ def dataLoad(filename):
             data=np.vstack((data,row))           
     return data[1:,:]
 #print(dataLoad('testforreals.txt'))
+data = dataLoad("test2.txt")
+
+def dataStatistics(data, statistic):
+    if statistic == 'Mean Temperature':
+    #takes the mean of the 0th column     
+        result = data[:,0].mean()
+    if statistic == 'Mean Growth rate': 
+        result = data[:,1].mean()
+    if statistic == 'Std Temperature':
+        result = data[:,0].std()
+    if statistic == 'Std Growth rate':
+        result = data[:,1].std()
+    if statistic == 'Rows':
+        result = len(data)
+    if statistic == 'Mean Cold Growth rate':
+    #coldData is a new array where all temps are less than 20    
+        coldData = data[data[:,0]< 20]
+    #gets the mean of the growth rate of the new array where all temps are less than 20    
+        result = coldData[:,1].mean()
+    if statistic == 'Mean Hot Growth rate':
+        hotData = data[data[:,0]>50]
+        result = hotData[:,1].mean()
+    return result
+
+print(dataStatistics(data, "Rows"))
